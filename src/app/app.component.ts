5cb23cc17fac0b28a50097c1828578fac36c457a
T <tamoit02@hs-esslingen.de>
2025-03-16 16:04:10 +0100

    First Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  template: '<router-outlet/>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'Messenger', component: LandingPageComponent },
+  { path: 'register', title: 'Register', component: RegisterComponent},
+  { path: 'login', title: 'Login', component: LoginComponent}, //canActivate: [AuthGuard]
+  { path: 'logout', title: 'Logout', component: LogoutComponent},
+  //add homepage
+];
