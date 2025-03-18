81fc12ba613fd0b5f450bd77d3e7d3eeb4000f8d
Dominik <147036891+doeiit00@users.noreply.github.com>
2025-03-18 16:24:43 +0100

    Redirect Works
    Co-Authored-By: T <149052166+tamoit02@users.noreply.github.com>

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   standalone: true,
@@ -7,11 +10,33 @@ import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
   imports: [
     RouterLink,
     RouterLinkActive,
-    RouterOutlet
+    RouterOutlet,
+    FormsModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  userID: string = '';
+  nickname: string = '';
+  fullname: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  register() {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const params = {
+      userid: this.userID,
+      password: this.password,
+      nickname: this.nickname,
+      fullname: this.fullname
+    }
+
+    this.http.get('https://www2.hs-esslingen.de/~melcher/map/chat/api/?request=register', { headers, params }).subscribe((res: any) => {
+      const token = res.token; // Adjust this line based on the actual response structure
+      this.authService.setToken(token);
+      console.log('Token saved:', token);
+    });
+  }
 }
